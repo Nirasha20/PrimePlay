0e6a243a4e4ee227f776bd1ef99bb867e55697a8
Nirasha <demelwgnd.22@uom.lk>
2025-11-21 22:02:07 +0530

    refactor: update layout and styling in index, enhance tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#030014',
+        secondary: '#1E192C',
+        light: {
+          100: '#FFFFFF',
+          200: '#F5F5F5',
+          300: '#E0E0E0',
+        },
+        dark: {
+          100: '#121212',
+          200: '#1E1E1E',
+          300: '#2C2C2C',
+        },
+        accent: '#00D1FF',
+      },
+    },
   },
   plugins: [],
 }
